ac00bb27525d7801177130777e5720b0d8672fa5
Gavin Ward <gbward@crimson.ua.edu>
2024-11-19 16:17:12 -0600

    merge and remove unneccesary files

--- routes/booksRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {createBook} from "../controllers/bookController.js";
-
-const router = express.Router();
-
-router.post('/', createBook);
-
-export default router;
--- routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { insertNewReview, patchOneReview } from '../controllers/reviewController.js'
-
-const router = express.Router();
-
-router.post('/', insertNewReview); //routes to the controller for displaying books
-router.patch('/:id', patchOneReview);
-export default router
